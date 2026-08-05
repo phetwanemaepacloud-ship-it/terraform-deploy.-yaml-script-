a367b4c63d0a9c307b3596a60d3cb5355e01ee05
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-05 16:30:33 +0200

    Update vpc..tf

--- vpc..tf
+++ vpc..tf
@@ -53,136 +53,87 @@ resource "aws_subnet" "public_subnet_az1" {
   }
 }
 
-
-
 resource "aws_subnet" "public_subnet_az2" {
+  vpc_id                  = #
+  cidr_block              = #
+  availability_zone       = #
+  map_public_ip_on_launch = #
 
-vpc_id = #
-
-cidr_block = #
-
-availability_zone = #
-
- map_public_ip_on_launch = #
-
-
-
-tags = {
-
-Name = #
-
-
+  tags = {
+    Name = #
+  }
+} 
 
 # Public route table routes traffic to the internet gateway 
-
 resource "aws_route_table" "public route_table" {
+  vpc_id = #
+  
+  route {
+   cidr_block = # 
+   gateway_id = #
+  }
 
-vpc_id = #
-
-route {
-
-cidr_block = # 
-
-gateway_id = #
-
-}
-
-tags = {
-
-Name = #
-
-}
-
+  tags = {
+    Name = #
+  }
+} 
 
 
 # Associate public subnets with public route table
-
 resource "aws_route_table_association" "public_subnet_azl_rt_association" { 
+  subnet_id      = #
+  route_table_id = #
+} 
 
-subnet_id = #
-
-route_table_id = #
-
-resource "aws_route_table_association" "public_subnet_az2_rt_association" { subnet_id
-
-route_table_id =
+resource "aws_route_table_association" "public_subnet_az2_rt_association" { 
+  subnet_id      = #
+  route_table_id = #
+} 
 
 # Private app subnets (application tier - ECS, EKS, EC2) 
-
 resource "aws_subnet" "private_app_subnet_az1" {
+  vpc_id                  = #
+  cidr_block              = #
+  availability_zone       = #
+  map_public_ip_on_launch = #
 
-vpc_id = #
-
-cidr_block = #
-
-availability_zone = #
-
-map_public_ip_on_launch = #
-
-
-
-tags = {
-
-Name = #
-
-}
-
-
+  tags = {
+    Name = #
+  }
+} 
 
 resource "aws_subnet" "private_app_subnet_az2" {
+  vpc_id                  = #
+  cidr_block              = #
+  availability_zone       = #
+  map_public_ip_on_launch = #
 
-}
-
-vpc_id = #
-
-cidr_block = #
-
-availability_zone = #
-
-map_public_ip_on_launch = #
-
-
-
-tags = { 
-
-Name = #
-
-}
-
+  tags = { 
+    Name = #
+  }
+} 
 
 
 # Private data subnets (database tier RDS, ElastiCache) 
-
 resource "aws_subnet" "private_data_subnet_az1" {
+  vpc_id                  = #
+  cidr_block              = #
+  availability_zone       = # 
+  map_public_ip_on_launch = #
 
-vpc_id = #
-
-cidr_block = #
-
-availability_zone = # map_public_ip_on_launch
-
-}
-
-tags
-
-Name = #
-
-}
+  tags = {
+  Name = #
+  }
+} 
 
 resource "aws_subnet" "private_data_subnet_az2" {
+  vpc_id                  = #
+  cidr_block              = #
+  availability_zone       = #
+  map_public_ip_on_launch = #
 
-vpc_id = #
-
-cidr_block = #
-
-availability_zone = #
-
-map_public_ip_on_launch = #
-
-tags = {
-
-Name = #
-
-}
-
+  tags = {
+    Name = #
+  }
+} 
 
