e7065b9badf232cfab9b7b88e23aa65dc8d0f2c7
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-07 14:39:33 +0200

    Create sns.tf

---
+++ sms.tf
@@ -0,0 +1,12 @@
+# SNS topic for notifications
+resource "aws_sns_topic" "user_updates" {
+  name = "${var.environment)-${var.project_name)-sns-topic"
+}
+
+# Email subscription to SNS topic
+resource "aws_sns_topic_subscription" "notification_topic" {
+  topic_arn = aws_sns_topic.user_updates.arn
+  protocol  = "email"
+  endpoint  = var.operator_email
+}
+
