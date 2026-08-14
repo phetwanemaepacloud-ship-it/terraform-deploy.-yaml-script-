5d6dab211b2141c0f797c6b1a3d55577c605bdef
phetwanemaepacloud-ship-it <phetwanemaepacloud@gmail.com>
2026-08-14 14:52:12 +0200

    Update nat-gateway.tf

--- nat-gateway.tf
+++ nat-gateway.tf
@@ -1,53 +1,14 @@
-# Elastic IP for NAT gateway 
-resource "aws_eip" "eip1" { 
-  domain= "vpc"
 
-  tags = {
-    Name = "$(var.environment)-eip-1"
-  } 
-} 
 
-# NAT gateway for private subnet internet access 
-resource "aws_nat_gateway" "nat_gateway_az1" { 
-  allocation_id = aws_eip.eip1.id
-  subnet_id     = aws_subnet.public_subnet_az1.id
-
-  tags = {
-    Name = "$(var.environment)-natgw-az1" 
-}
-
-# Ensure the NAT Gateway is created after the Internet Gateway 
-depends_on = [aws_internet_gateway.internet_gateway]
-} 
-
-# Private route table routes traffic through NAT gateway 
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.vpc.id 
-
-  route {
-    cidr_block     = 0.0.0.0/0
-    nat_gateway_id = aws_nat_gateway.nat_gateway_az1.id
-  }
-
-  tags = {
-    Name = "$(var.environment)-rtb-private
-  }
-} 
-
-# Associate private subnets with private route table
-resource "aws_route_table_association" "private_app_subnet_azl_rt_association" { 
-subnet_id      = aws_subnet.private_app_subnet_az1.id
-route_table_id = aws_route_table.private_route_table.id
-} 
-resource "aws_route_table_association" "private_data_subnet_azl_rt_association" { 
-subnet_id      = aws_subnet.private_data_subnet_az1.id
-route_table_id = aws_route_table.private_route_table.id
-} 
-resource "aws_route_table_association" "private_app_subnet_az2_rt_association" { 
-subnet_id      = aws_subnet.private_app_subnet_az2.id
-route_table_id = aws_route_table.private_route_table.id
-} 
-resource "aws_route_table_association" "private_data_subnet_az2_rt_association" { 
-subnet_id      = aws_subnet.private_data_subnet_az1.id
-route_table_id = aws_route_table.private_route_table.id
+"Create Nat Gateway 
+module "nat-gateway" {
+  source                     =
+  environment                =
+  public_subnet_azl.Id       =
+  internet_gateway           =
+  vpc_id                     =
+  private_app_subnet_az1.Id  =
+  private_app_subnet_az1.Id  =
+  private_data_subnet_az1.id =
+  private_data_subnet_az2.id =
 } 
